97dc729a256f8ccfe767595d5721d8b84bc5aa1e
Dongho Kim <k45414254@gmail.com>
2021-06-22 20:21:12 +0900

    필터링 추가

--- app/src/main/java/com/roomedia/dawn_down_alarm/presentation/view/adapter/AppListAdapter.kt
+++ app/src/main/java/com/roomedia/dawn_down_alarm/presentation/view/adapter/AppListAdapter.kt
@@ -10,8 +10,15 @@ import com.roomedia.dawn_down_alarm.databinding.ItemAppListBinding
 import com.roomedia.dawn_down_alarm.entity.AppAndKeywords
 import com.roomedia.dawn_down_alarm.presentation.view.fragment.AppListFragment
 import com.roomedia.dawn_down_alarm.presentation.view.fragment.EditKeywordBottomSheetDialogFragment
+import io.github.bangjunyoung.KoreanTextMatcher
+import java.util.*
 
 class AppListAdapter(private val fragment: AppListFragment) : ListAdapter<AppAndKeywords, AppListAdapter.ViewHolder>(DiffUtilCallback) {
+
+    private var totalList: List<AppAndKeywords>? = emptyList()
+    private var query: String? = null
+    private var enabledFilter = false
+    private var hasKeywordsFilter = false
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AppListAdapter.ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
@@ -21,6 +28,45 @@ class AppListAdapter(private val fragment: AppListFragment) : ListAdapter<AppAnd
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(getItem(position), position)
+    }
+
+    override fun submitList(list: List<AppAndKeywords>?) {
+        var filteredList = totalList
+        if (query != null) {
+            val query = query!!.toLowerCase(Locale.ROOT)
+            val matcher = KoreanTextMatcher(query)
+            filteredList = filteredList?.filter {
+                val appName = it.app?.appName?.toLowerCase(Locale.ROOT)
+                val keywords = it.keywords.joinToString(separator = "\n").toLowerCase(Locale.ROOT)
+                appName?.contains(query) == true
+                        || keywords.contains(query)
+                        || matcher.match(appName).success()
+                        || matcher.match(keywords).success()
+            }
+        }
+        if (enabledFilter) {
+            filteredList = filteredList?.filter { it.app?.isEnabled == true }
+        }
+        if (hasKeywordsFilter) {
+            filteredList = filteredList?.filter { it.keywords.isNotEmpty() }
+        }
+        super.submitList(filteredList)
+    }
+
+    fun updateList(list: List<AppAndKeywords>?) {
+        totalList = list
+        submitList(list)
+    }
+
+    fun updateQuery(query: String?) {
+        this.query = query
+        submitList(totalList)
+    }
+
+    fun updateFilter(enabledFilter: Boolean = this.enabledFilter, hasKeywordsFilter: Boolean = this.hasKeywordsFilter) {
+        this.enabledFilter = enabledFilter
+        this.hasKeywordsFilter = hasKeywordsFilter
+        submitList(totalList)
     }
 
     fun showBottomSheetDialog(view: View) {
